28db2037b151a3f8d5d3494c3bc7c14710ea5493
James Wild <James Wild>
2016-10-23 08:38:08 +0100

    Added a font.

--- src/engine/boot.js
+++ src/engine/boot.js
@@ -1,8 +1,46 @@
 var sharedContent
 
+var fontBig = {
+    lineSpacing: 14,
+    kerning: {
+        "default": 6,
+        "i": 2,
+        "j": 2,
+        "c": 5,
+        "f": 4,
+        "t": 5,
+        "r": 5,
+        "k": 5,
+        "l": 4,
+        " ": 3,
+        ":": 2,
+        ";": 3,
+        "!": 2,
+        "?": 5,
+        "(": 4,
+        ")": 4,
+        "[": 4,
+        "]": 4,
+        "{": 4,
+        "}": 4,
+        "\t": 10,
+        "-": 4,
+        "'": 1,
+        "\"": 4,
+        "=": 5,
+        "%": 5,
+        "/": 5,
+        "\\": 5,
+        ",": 3,
+        "I": 2,
+        "|": 2
+    }
+}
+
 function SprigganBoot(contentManager) {
     contentManager.add(SprigganSpriteSheet, "character")
     contentManager.add(SprigganSpriteSheet, "items/icons")
+    contentManager.add(SprigganSpriteSheet, "fontBig")
     sharedContent = contentManager
     return function() {
         var savegame = {
