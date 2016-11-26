1db8325fa9f382cd48a109ae340e81ddc0cbd40a
James Wild <James Wild>
2016-11-26 17:38:42 +0000

    Added a function to find the "facing direction" between two points on the screen.

--- src/engine/misc.js
+++ src/engine/misc.js
@@ -1,3 +1,17 @@
 function Capitalize(str) {
     return str[0].toUpperCase() + str.slice(1)
 }
+
+function DirectionBetween(fromX, fromY, toX, toY) {    
+    if (Math.abs(fromX - toX) > Math.abs(fromY - toY)) {
+        if (toX > fromX)
+            return "right"
+        else
+            return "left"
+    } else {
+        if (toY > fromY)
+            return "down"
+        else
+            return "up"
+    }
+}
