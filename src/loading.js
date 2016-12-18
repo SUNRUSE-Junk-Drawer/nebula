b271be7b997f3d21842ed3cd1444c7761e26837b
James Wild <James Wild>
2016-12-18 15:47:34 +0000

    The loading sheet no longer includes a fixed screen resolution, and the logo is now shown on the first loading screen.

--- src/loading.js
+++ src/loading.js
@@ -1,10 +1,23 @@
+var firstLoadingScreen = true
+
 function ShowLoadingScreen(then) {
-    var viewport = new SprigganViewport(screenWidth, screenHeight, "right", "bottom")
+    var textViewport = new SprigganViewport(screenWidth, screenHeight, "right", "bottom")
     
-    var nowLoading = new SprigganSprite(viewport, sharedContent, "logo")
+    var nowLoading = new SprigganSprite(textViewport, sharedContent, "logo")
     nowLoading.loop("nowLoading")
-    var ellipsis = new SprigganSprite(viewport, sharedContent, "logo")
+    nowLoading.move(screenWidth, screenHeight)
+    var ellipsis = new SprigganSprite(textViewport, sharedContent, "logo")
     ellipsis.hide()
+    ellipsis.move(screenWidth, screenHeight)
+    
+    var logoViewport
+    if (firstLoadingScreen) {
+        logoViewport = new SprigganViewport(screenWidth, screenHeight)
+        var logo = new SprigganSprite(logoViewport, sharedContent, "logo")
+        logo.move(screenWidth / 2, screenHeight / 2)
+        logo.loop("logo")
+        firstLoadingScreen = false
+    }
     
     var contentManager = new SprigganContentManager({
         progress: function(loaded, total) {
@@ -20,7 +33,8 @@ function ShowLoadingScreen(then) {
             }
         },
         loaded: function() {
-            viewport.dispose()
+            textViewport.dispose()
+            if (logoViewport) logoViewport.dispose()
             then()
         }
     })
